15b9f8666086485981c761e30a60e72744972481
UrakamiHarumi <hrm132603@gmail.com>
2025-02-01 01:24:41 +0900

    Lesson_ECsite

---
+++ src/main/java/jp/co/internous/ecsite/model/domain/MstGoods.java
@@ -0,0 +1,47 @@
+package jp.co.internous.ecsite.model.domain;
+
+import java.sql.Timestamp;
+
+public class MstGoods {
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	private String goodsName;
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName=goodsName;
+	}
+	
+	private int price;
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price=price;
+	}
+	
+	private Timestamp createAt;
+	public Timestamp getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Timestamp createAt) {
+		this.createAt=createAt;
+	}
+	
+	private Timestamp updateAt;
+	public Timestamp getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(Timestamp updateAt) {
+		this.updateAt=updateAt;
+	}
+
+}
